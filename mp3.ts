b4d29cfec9040a9f4b9546008ba08e8c233e65a8
ardubits-kwm <124767036+ardubits-kwm@users.noreply.github.com>
2023-03-22 11:02:14 +0800

    Update mp3.ts

--- mp3.ts
+++ mp3.ts
@@ -3,7 +3,7 @@
 
 //% weight=100 color=#64C800 icon="\u272a" block="MP3"
 namespace MP3 {
-    let commandBuffer: Buffer = undefined;
+
 
     /**
      * init mp3
@@ -11,6 +11,7 @@ namespace MP3 {
     //% blockId="Init_mp3" block="connect MP3 device with MP3 RX attached to %mp3RX | and MP3 TX to %mp3TX"
     //% weight=50 
     export function init_mp3(mp3RX: SerialPin, mp3TX: SerialPin): void {
+        let commandBuffer: Buffer = undefined;
         serial.redirect(mp3RX as number,mp3TX as number,BaudRate.BaudRate9600)
     }
 
