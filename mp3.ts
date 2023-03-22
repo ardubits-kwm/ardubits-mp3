92573b9e701d0291865c6613bca4ee66062737cc
ardubits-kwm <124767036+ardubits-kwm@users.noreply.github.com>
2023-03-22 16:06:36 +0800

    Update mp3.ts

--- mp3.ts
+++ mp3.ts
@@ -8,7 +8,7 @@ namespace MP3 {
     /**
      * init mp3
      */
-    //% blockId="Init_mp3" block="connect MP3 device with MP3 RX attached to %mp3RX | and MP3 TX to %mp3TX"
+    //% blockId="Init_mp3" block="init MP3 RX  %mp3RX |  TX  %mp3TX"
     //% weight=50 
     export function init_mp3(mp3RX: SerialPin, mp3TX: SerialPin):void {
         serial.redirect(mp3RX as number,mp3TX as number,BaudRate.BaudRate9600);
