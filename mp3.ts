8014a9d5035755a40a3e015fa46a85898032a449
ardubits-kwm <124767036+ardubits-kwm@users.noreply.github.com>
2023-03-31 15:45:11 +0800

    Add files via upload

---
+++ mp3.ts
@@ -0,0 +1,35 @@
+// MakerBit Serial MP3 blocks supporting Catalex Serial MP3 1.0
+
+
+//% weight=100 color=#64C800 icon="\u272a" block="MP3"
+namespace MP3 {
+    let commandBuffer: Buffer = undefined
+
+
+
+    /**
+     * init mp3
+     */
+    //% blockId="INIT_MP3" block="init MP3 RX|%rx  TX|%tx"
+    //% weight=50 
+    export function initMp3(rx: SerialPin, tx: SerialPin):void {
+        serial.redirect(rx as number,tx as number,BaudRate.BaudRate9600)
+    }
+
+    /**
+     * play mp3
+     */
+    //% blockId="PLAY_MP3" block="play mp3 |%PrevNext"
+    //% weight=49 
+    export function play(num: PrevNext): void{
+        let buf = pins.createBuffer(6);
+        buf[0] = 0x7e;
+        buf[1] = 0x04;
+        buf[2] = 0x41;
+        buf[3] = 0x00;
+        buf[4] = num;
+        buf[5] = 0xef;
+        serial.writeBuffer(buf);
+    }
+
+}
